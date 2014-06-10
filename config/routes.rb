4677e74db10bfcce907cc410998189fbe05e9b4a
I Rastkhadivmasouleh <iman.khadiv@gmail.com>
2014-06-10 19:44:35 +0100

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/welcome'
+  root 'home#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
