51d5fa467472071688cdec97bc94b7960c2da4f2
I Rastkhadivmasouleh <iman.khadiv@gmail.com>
2014-06-20 09:50:00 +0100

    changed swimmer model

--- app/models/swimmer.rb
+++ app/models/swimmer.rb
@@ -1,5 +1,6 @@
 class Swimmer < ActiveRecord::Base
   belongs_to :user
+  has_and_belongs_to_many :parents
   has_one :address, :as => :addressable
   accepts_nested_attributes_for :address, allow_destroy: true
 end
