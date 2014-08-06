0681675f89e29bb0481c0a931f097c6a00c9c105
I Rastkhadivmasouleh <iman.khadiv@gmail.com>
2014-08-06 22:30:15 +0100

    pictures added

--- app/assets/javascripts/parents.js
+++ app/assets/javascripts/parents.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
         if (v){
             return true
         }else {
+//            $('#check').remove()
+//            $('#check').remove("<div class='alert alert-danger' role='alert'><p>Not Valid</p><br/></div>");
+            $('#check .alert').remove();
             $('#check').append("<div class='alert alert-danger' role='alert'><br/><p>Please add your child</div></p>");
             return false;
         }
