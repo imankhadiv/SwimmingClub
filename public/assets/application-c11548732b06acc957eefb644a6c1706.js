c28f829bd0559b76db3fe4c67eddd3914cae3311
I Rastkhadivmasouleh <iman.khadiv@gmail.com>
2014-08-08 00:16:52 +0100

    assets cleaned

--- public/assets/application-c11548732b06acc957eefb644a6c1706.js
+++ public/assets/application-c11548732b06acc957eefb644a6c1706.js
@@ -30714,7 +30714,10 @@ $(document).ready(function(){
         if (v){
             return true
         }else {
-            $('#check').append("<div class='alert alert-danger' role='alert'><br/><p>Plase add your child</div></p>");
+//            $('#check').remove()
+//            $('#check').remove("<div class='alert alert-danger' role='alert'><p>Not Valid</p><br/></div>");
+            $('#check .alert').remove();
+            $('#check').append("<div class='alert alert-danger' role='alert'><br/><p>Please add your child</div></p>");
             return false;
         }
     });
