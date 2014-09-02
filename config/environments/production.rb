eadba15788fd085ce318af7f89867de4fb848728
I Rastkhadivmasouleh <iman.khadiv@gmail.com>
2014-09-02 10:10:56 +0100

    modified email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,8 @@ Rails.application.configure do
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
 
-  config.action_mailer.default_url_options = { host: 'http://www.swimmingclub.nestednet.com' }
+  # config.action_mailer.default_url_options = { host: 'http://www.swimmingclub.nestednet.com' }
+  config.action_mailer.default_url_options = { host: 'http://swimmingclub.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
